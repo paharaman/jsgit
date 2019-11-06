1e98af3776318f20e26ae6706f44def705b2480e
paharaman <56583504+paharaman@users.noreply.github.com>
2019-11-06 20:00:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,11 @@
 //import {Backend} from "/backend.js"
 //const api = new Backend;
-//api.setBaseURL("https://api.learnjavascript.online/demo/shopping-list.json");
-const button = document.querySelector("button");
-const ul = document.querySelector("#listings");
 
-const displayElement = element => {
-    ul.insertAdjacentHTML("beforeend", `<li>${element}</li>`);
+class MyFirstWebComponent extends HTMLElement {
+    constructor(){
+        super();
+        this.table = document.querySelector("#tableHeader");
+    }
 }
 
-const getInfo = () => {
-    fetch("https://api.learnjavascript.online/demo/shopping-list.json").then(response => response.json())
-    .then(data => {
-        data.forEach(element => {
-            displayElement(element);
-        });
-
-    })
-
-    button.addEventListener("click", e => {
-        e.preventDefault();
-        getInfo();
-    })
-
-}
+window.customElements.define("app-table", MyFirstWebComponent);
