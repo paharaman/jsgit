ead2be10ef311cabd4dfe9e5717e13a68176ab2a
paharaman <56583504+paharaman@users.noreply.github.com>
2019-11-08 05:53:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-//import {Backend} from "/backend.js"
-//const api = new Backend;
-
-class MyFirstWebComponent extends HTMLElement {
+class AppTextPara extends HTMLElement {
     constructor(){
         super();
-        this.table = document.querySelector("#tableHeader");
+        const shadowRoot = this.attachShadow({mode: 'open'});
+
+        let wrapper = document.createElement("span");
+        wrapper.setAttribute("class", "wrapper");
+        wrapper.innerHTML = "Добре дошли!";
+
+        let style = document.createElement("style");
+        style.textContent = `.wrapper{
+            font-weight : bold;
+            margin-left: 20px;
+        }`
+
+        shadowRoot.appendChild(style);
+        shadowRoot.appendChild(wrapper);
+    }   
+} 
+window.customElements.define("app-text-para", AppTextPara); 
+
+class AppAnotherText extends HTMLElement {
+    constructor(){
+        super();
+        const shadow = this.attachShadow({mode: "open"});
+        const anotherText = document.querySelector("#another-text");
+        shadow.appendChild(anotherText.content.cloneNode(true));
     }
 }
-
-window.customElements.define("app-table", MyFirstWebComponent);
+window.customElements.define("app-another-text", AppAnotherText); 
